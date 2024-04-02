6db8ce153215b3d36c0cdbb4eecf41713dace086
DESKTOP-7FL9E6G\User <hibin7014@gmail.com>
2024-04-02 17:07:02 +0900

    파이어뱃

--- Firebat.cpp
+++ Firebat.cpp
@@ -1,8 +1,33 @@
-#include "Firebat.h"
 #include <iostream>
+
+#include "Firebat.h"
 using namespace std;
 
-Firebat::Firebat()
+
+class Firebat : public Unit
 {
-	power = 2;
-}
+private:
+
+public:
+
+	void doMove()
+	{
+		cout << "15 speed 이동" << endl;
+	}
+
+	void doRun()
+	{
+
+		cout << "30 speed 질주 " << endl;
+	}
+	void doStatus()
+	{
+
+		cout << power<<"의 공격력으로 공격 " << endl;
+	}
+
+
+
+
+
+};
